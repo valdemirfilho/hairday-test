54bc3b00db26ddccff3e8417345e6b14a51cc210
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-26 04:21:42 -0300

    renderizando horários.

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,13 +1,21 @@
 import dayjs from 'dayjs';
 import { openingHours } from '../../utils/opening-hours';
 
+
+const hours = document.getElementById('hours')
 export function hoursLoad({ date }) {
   const opening = openingHours.map(hour => {
     const [scheduleHour, ] = hour.split(':');
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs());
-    console.log({
+    return{
       hour,
       available: isHourPast
-    })
-  })  
+    }
+  })
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement('li');
+    li.classList.add('hour', available ? 'hour-available' : 'hour-unavailable');
+    li.textContent = hour;
+    hours.append(li);
+  })
 }
