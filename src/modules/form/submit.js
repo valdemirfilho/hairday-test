789c1484cd403b123bbc7e21ea7c5aabe399b642
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-26 04:57:07 -0300

    enviando o horário do agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,7 +1,8 @@
 import dayjs from "dayjs";
-const form = document.querySelector('form');
 
+const form = document.querySelector('form');
 const selectedDate = document.getElementById("date");
+const clientName = document.getElementById("client");
 
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 
@@ -9,5 +10,25 @@ selectedDate.value = inputToday
 selectedDate.min = inputToday
 form.onsubmit = (event) => {
   event.preventDefault()
-  console.log("enviou")
+  try {
+    const name = clientName.value.trim()
+    if(!name) {
+      alert("Por favor, informe o nome do cliente")
+    }
+    const hourSelected = document.querySelector('.hour-selected')
+    
+    if(!hourSelected) {
+      alert("Por favor, selecione um horário")
+    }
+    const [hour] = hourSelected.innerText.split(':')
+    
+    const when = dayjs(selectedDate.value).add(hour, 'hour')
+    
+    const id = new Date().getTime()
+
+    console.log({ id, name, when })
+  }catch (error) {
+    alert("Não foi possível realizar o agendamento")
+    console.log(error)
+  }
 }
