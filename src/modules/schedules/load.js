e4c96ab7168707725106f14d6cbb99f6345aa943
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-27 08:28:12 -0300

    renderizando horários agendados.

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,13 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
 import { hoursLoad } from "../form/hours-load";
+import { schedulesShow } from "./show";
 
 
 const selectedDate =  document.getElementById("date");
 export async function schedulesDay() {
   const date = selectedDate.value
   const dailySchedules = await scheduleFetchByDay({date})
-  console.log(dailySchedules)
+  
+  schedulesShow({dailySchedules})
   hoursLoad({date})
 }
