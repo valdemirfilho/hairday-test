33ca26731cdc22bbe686bd8009a260241b9f9044
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-27 08:54:34 -0300

    remoção do agendamento na DOM

---
+++ src/modules/schedules/cancel.js
@@ -0,0 +1,16 @@
+const periods = document.querySelectorAll(".period")
+
+periods.forEach(period => {
+  period.addEventListener("click", (event) => {
+    if (event.target.classList.contains("cancel-icon")) {
+      const item = event.target.closest("li")
+      const {id} = item.dataset
+      if(id) {
+        const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?")
+        if(isConfirm) {
+          item.remove()
+        }
+      }
+    }
+  })
+})
