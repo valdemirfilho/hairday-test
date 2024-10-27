5fdc7efa18b8fe7e86cb485960ddf859892f2439
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-27 09:08:30 -0300

    removendo agendamentos da api, com ERRO 404.

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,14 +1,18 @@
+import { scheduleCancel } from "../../services/schedule-cancel"
+import { schedulesDay } from "./load"
+
 const periods = document.querySelectorAll(".period")
 
 periods.forEach(period => {
-  period.addEventListener("click", (event) => {
+  period.addEventListener("click", async (event) => {
     if (event.target.classList.contains("cancel-icon")) {
       const item = event.target.closest("li")
       const {id} = item.dataset
       if(id) {
         const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?")
         if(isConfirm) {
-          item.remove()
+          await scheduleCancel({id})
+          schedulesDay()
         }
       }
     }
