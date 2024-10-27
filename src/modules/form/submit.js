efe977ae9a6a88c969367991d13a91e1b6fa2c7b
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-27 07:07:16 -0300

    registrando um novo agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import { scheduleNew } from "../../services/schedule-new";
 
 const form = document.querySelector('form');
 const selectedDate = document.getElementById("date");
@@ -8,7 +9,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 
 selectedDate.value = inputToday
 selectedDate.min = inputToday
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault()
   try {
     const name = clientName.value.trim()
@@ -26,7 +27,7 @@ form.onsubmit = (event) => {
     
     const id = new Date().getTime()
 
-    console.log({ id, name, when })
+    await scheduleNew({ id, name, when })
   }catch (error) {
     alert("Não foi possível realizar o agendamento")
     console.log(error)
